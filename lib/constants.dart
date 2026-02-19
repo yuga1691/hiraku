371fd29c635cc7eeea4f6e94f70ea44ecf93bab6
yuga1691 <zerogravity1691@gmail.com>
2026-02-19 10:55:28 +0900

    DiscordURL張り替え

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
 const String kTeamJoinUrl = 'https://groups.google.com/g/hiraku_app';
 const String kTeamJoinEmail = 'hiraku_app@googlegroups.com';
-const String kDiscordInviteUrl = 'https://discord.gg/SzekgtpP';
+const String kDiscordInviteUrl = 'https://discord.gg/d7rxuMMXta';
